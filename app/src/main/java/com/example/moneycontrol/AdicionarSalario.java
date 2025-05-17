4f940419ff311a62c2eed08c70a86fa33d1a9446
SauloOliveira358 <saulooliveiram358@gmail.com>
2025-05-17 18:32:39 -0300

    Telas Adicionadas Salario e Gasto

---
+++ app/src/main/java/com/example/moneycontrol/AdicionarSalario.java
@@ -0,0 +1,13 @@
+package com.example.moneycontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdicionarSalario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_salario);
+    }
+}
--- app/src/main/java/com/example/moneycontrol/MainActivity.java
+++ app/src/main/java/com/example/moneycontrol/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.moneycontrol;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,16 +8,28 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-private Button adicionar_Gasto;
+private Button adicionar_Gasto, adicionar_Salario;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         adicionar_Gasto = findViewById(R.id.IdBtnAdicionarGastoMain);
+        adicionar_Salario = findViewById(R.id.IdBtnAdicionarSalarioMain);
+        //Botao Mudar a tela Gasto
         adicionar_Gasto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(),AdicionarGastos.class);
+                startActivity(intent);
+            }
+        });
+
+        //botao mudar a tela salario
+        adicionar_Salario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),AdicionarSalario.class);
                 startActivity(intent);
             }
         });
